201f0c34822c11fa41b3baebcecac746f8c334ab
StrategFBI <94102957+StrategFBI@users.noreply.github.com>
2023-04-23 16:07:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const winner = document.getElementById('winner');
+const cells = document.querySelectorAll('.cell');
+const restartButton = document.getElementById('restart-button');
+const turnInfo = document.getElementById('turn-info');
+
+const players = {
+    x: 'x',
+    o: 'o'
+}
+
+let currentPlayer = '';
+let isGameRunning = false;
+let boardState = Array(9).fill('');
+const winLines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [0,4,8],
+    [2,5,8],
+    [1,4,7],
+    [2,4,6],
+]
+
+
+function initalizeGame() {
+    cells.forEach(cell => cell.addEventListener('click', clickCell));
+    restartButton.addEventListener('click', restartGame)
+}
+
+
+function startGame() {
+    isGameRunning = true;
+    cells.forEach(cell => cell.textContent = '')
+    winner.textContent = ''
+    currentPlayer = players.x
+    turnInfo.textContent = `It's ${currentPlayer}'s turn`
+    boardState = Array(9).fill('');
+}
+
+function clickCell() {
+    if (!isGameRunning) return;
+    if (this.textContent) return;
+    this.textContent = currentPlayer;
+    const cellIndex = this.dataset.cellIndex;
+    boardState[cellIndex] = currentPlayer;
+    if (checkGameOver()) {
+        return finishGame()
+    }
+    currentPlayer = (currentPlayer === players.x) ? players.o : players.x
+    turnInfo.textContent = `It's ${currentPlayer}'s turn`
+    
+}
+
+function checkGameOver() {
+    for (const line of winLines) {
+        if (checkLine(line)) {
+            winner.textContent = `${currentPlayer} won`
+            return true
+        }
+    }
+    if (!boardState.includes('')) {
+        winner.textContent = 'Draw'
+    }
+}
+
+function finishGame() {
+    isGameRunning = false
+    turnInfo.textContent = '';
+}
+
+function restartGame() {
+    finishGame()
+    startGame()
+}
+
+function checkLine(line) {
+    const [a,b,c] = line;
+    const cellA = boardState[a];
+    const cellB = boardState[b];
+    const cellC = boardState[c];
+
+    if ([cellA,cellB, cellC].includes('')) return false;
+
+    return cellA === cellB && cellB === cellC
+
+}
+
+window.addEventListener('load', ()=> { initalizeGame(),startGame()})
